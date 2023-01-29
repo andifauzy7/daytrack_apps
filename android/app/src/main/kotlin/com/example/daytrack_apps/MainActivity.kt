88908b33b6dde49711d0beba19f6f64d282b0757
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2023-01-29 13:06:57 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/daytrack_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.daytrack_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/injection/injection.dart
@@ -0,0 +1,28 @@
+import 'package:daytrack_apps/core/network/network_info.dart';
+import 'package:daytrack_apps/core/service_locator/service_locator.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class Injection {
+  Future<void> initialize() async {
+    await _registerCoreDependencies();
+    _registerDependencies();
+  }
+
+  void _registerDependencies() {
+    // TODO: Register Feature Dependency : Eg. NumberTriviaDependencies();
+  }
+
+  Future<void> _registerCoreDependencies() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sl.registerLazySingleton(() => http.Client());
+    sl.registerLazySingleton(() => InternetConnectionChecker());
+    sl.registerLazySingleton(() => sharedPreferences);
+    sl.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(
+        connectionChecker: sl(),
+      ),
+    );
+  }
+}
